58384380375f055d6d71b38ed69da18881c585c0
Joe Scharf <joe@joescharf.com>
2022-06-09 06:49:02 -0600

    initial commit with sample tailwind working

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joescharf/gowind/v2
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, nil)
+}
+
+func main() {
+	render()
+	http.HandleFunc("/", indexHandler)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("templates/css"))))
+	http.ListenAndServe(":3000", nil)
+}
+
+// https://betterprogramming.pub/how-to-generate-html-with-golang-templates-5fad0d91252
+func render() {
+	allFiles := []string{"index.html"}
+
+	var allPaths []string
+	for _, tmpl := range allFiles {
+		allPaths = append(allPaths, "./templates/"+tmpl)
+	}
+
+	templates := template.Must(template.ParseFiles(allPaths...))
+
+	var processed bytes.Buffer
+	templates.ExecuteTemplate(&processed, "page", nil)
+
+	outputPath := "./dist/index.html"
+	f, _ := os.Create(outputPath)
+	w := bufio.NewWriter(f)
+	w.WriteString(string(processed.Bytes()))
+	w.Flush()
+}
